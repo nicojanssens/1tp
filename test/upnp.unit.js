717680d76cb83b66178deff9075c7d09b875b9a8
Nico Janssens <nico.b.janssens@gmail.com>
2016-06-20 21:56:38 +0200

    put UPnP tests behind flag -- to deactivate when executing tests on CircleCI

--- test/upnp.unit.js
+++ test/upnp.unit.js
@@ -2,6 +2,7 @@
 
 var ipAddresses = require('../src/nat/ip-addresses')
 var upnp = require('../src/nat/upnp')
+var yn = require('yn')
 
 var chai = require('chai')
 var chaiAsPromised = require('chai-as-promised')
@@ -11,74 +12,77 @@ chai.use(chaiAsPromised)
 chai.should()
 
 var myPublicIpAddress
+var executeUpnpTests = yn(process.env.EXECUTE_UPNP_TESTS)
 
-describe('#NAT-UPNP', function () {
-  this.timeout(10000)
+if (executeUpnpTests) {
+  describe('#NAT-UPNP', function () {
+    this.timeout(10000)
 
-  before(function (done) {
-    ipAddresses.getPublicIpAddressP()
-      .then(function (ip) {
-        myPublicIpAddress = ip
-        done()
+    before(function (done) {
+      ipAddresses.getPublicIpAddressP()
+        .then(function (ip) {
+          myPublicIpAddress = ip
+          done()
+        })
+        .catch(function (error) {
+          assert(false, 'Could not retrieve public address before running tests. ' + error)
+        })
       })
-      .catch(function (error) {
-        assert(false, 'Could not retrieve public address before running tests. ' + error)
-      })
+
+    it('should return my public ip address', function () {
+      return upnp.getPublicGWAddressP().should.eventually.equal(myPublicIpAddress)
+    })
+
+    it('should map UDP port 65532 to 65532 and delete it afterwards', function () {
+      var pmargs = {}
+      pmargs.public = {}
+      pmargs.public.port = 65532
+      var myMapping
+      return upnp.mapPublicPortP(pmargs)
+        .then(function (args) {
+          myMapping = args
+          return upnp.getPortMappingsP()
+        })
+        .then(function (currentMappings) {
+          expect(_pmMatch(myMapping, currentMappings)).to.be.true
+          return upnp.unmapPublicPortP(myMapping)
+        })
+        .then(function () {
+          return upnp.getPortMappingsP()
+        })
+        .then(function (currentMappings) {
+          expect(_pmMatch(myMapping, currentMappings)).to.be.false
+        })
+    })
+
+    it('should map TCP port 65534 to 65533, using custom description and ttl = 2 minutes, and delete it afterwards', function () {
+      var pmargs = {}
+      pmargs.public = {}
+      pmargs.private = {}
+      pmargs.private.port = 65534
+      pmargs.public.port = 65533
+      pmargs.protocol = 'TCP'
+      pmargs.ttl = 120
+      pmargs.description = 'funky:test'
+      var myMapping
+      return upnp.mapPublicPortP(pmargs)
+        .then(function (args) {
+          myMapping = args
+          return upnp.getPortMappingsP()
+        })
+        .then(function (currentMappings) {
+          expect(_pmMatch(myMapping, currentMappings)).to.be.true
+          return upnp.unmapPublicPortP(myMapping)
+        })
+        .then(function () {
+          return upnp.getPortMappingsP()
+        })
+        .then(function (currentMappings) {
+          expect(_pmMatch(myMapping, currentMappings)).to.be.false
+        })
+    })
   })
-
-  it('should return my public ip address', function () {
-    return upnp.getPublicGWAddressP().should.eventually.equal(myPublicIpAddress)
-  })
-
-  it('should map UDP port 65532 to 65532 and delete it afterwards', function () {
-    var pmargs = {}
-    pmargs.public = {}
-    pmargs.public.port = 65532
-    var myMapping
-    return upnp.mapPublicPortP(pmargs)
-      .then(function (args) {
-        myMapping = args
-        return upnp.getPortMappingsP()
-      })
-      .then(function (currentMappings) {
-        expect(_pmMatch(myMapping, currentMappings)).to.be.true
-        return upnp.unmapPublicPortP(myMapping)
-      })
-      .then(function () {
-        return upnp.getPortMappingsP()
-      })
-      .then(function (currentMappings) {
-        expect(_pmMatch(myMapping, currentMappings)).to.be.false
-      })
-  })
-
-  it('should map TCP port 65534 to 65533, using custom description and ttl = 2 minutes, and delete it afterwards', function () {
-    var pmargs = {}
-    pmargs.public = {}
-    pmargs.private = {}
-    pmargs.private.port = 65534
-    pmargs.public.port = 65533
-    pmargs.protocol = 'TCP'
-    pmargs.ttl = 120
-    pmargs.description = 'funky:test'
-    var myMapping
-    return upnp.mapPublicPortP(pmargs)
-      .then(function (args) {
-        myMapping = args
-        return upnp.getPortMappingsP()
-      })
-      .then(function (currentMappings) {
-        expect(_pmMatch(myMapping, currentMappings)).to.be.true
-        return upnp.unmapPublicPortP(myMapping)
-      })
-      .then(function () {
-        return upnp.getPortMappingsP()
-      })
-      .then(function (currentMappings) {
-        expect(_pmMatch(myMapping, currentMappings)).to.be.false
-      })
-  })
-})
+}
 
 function _pmMatch (myMapping, returnedMappings) {
   var match = false
