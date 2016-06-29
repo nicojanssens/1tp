9be1cbd25d2bb6e7c35ec36994c31021c2678471
Nico Janssens <nico.b.janssens@gmail.com>
2016-06-29 10:31:10 +0200

    added function to verify if object has no properties of its own

--- src/utils.js
+++ src/utils.js
@@ -56,5 +56,36 @@ var timeoutResolvePromise = function (promise, ms, callback) {
   return deferred.promise
 }
 
+/** from http://stackoverflow.com/questions/4994201/is-object-empty */
+
+// Speed up calls to hasOwnProperty
+var hasOwnProperty = Object.prototype.hasOwnProperty
+
+var isEmpty = function (obj) {
+    // null and undefined are "empty"
+    if (obj === null) {
+      return true
+    }
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0) {
+      return false
+    }
+    if (obj.length === 0) {
+      return true
+    }
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
+}
+
 module.exports.mixinEventEmitterErrorFunction = mixinEventEmitterErrorFunction
 module.exports.timeoutResolvePromise = timeoutResolvePromise
+module.exports.isEmpty = isEmpty
