4901238271ee8775ad7d25e3500e2bb02172aac8
Nico Janssens <nico.b.janssens@gmail.com>
2016-07-23 22:31:33 +0200

    updated README + added example script

--- examples/local.js
+++ examples/local.js
@@ -1,31 +1,8 @@
-[![CircleCI](https://circleci.com/gh/MicroMinion/1tp.svg?style=svg)](https://circleci.com/gh/MicroMinion/1tp)
-
-# 1tp
-One transport protocol to rule them all -- offering net socket abstraction on top of various communication protocols
-
-## Summary
-Goal of 1tp is to offer a single solution for connecting any two available endpoints -- public, private, mobile, located behind NAT boxes, ... To accomplish this, 1tp acts as a wrapper around various existing transports such as UDP, TCP and TURN (more to come in later releases). The internal details of these transports are concealed via an API similar to node's net API, using sockets and streams. Furthermore, 1tp always tries to use the 'cheapest' transport when establishing a connection between two endpoints. If both endpoints are sharing the same network, for instance, then 1tp will setup a UDP or a TCP connection. If two endpoints are located behind symmetric NAT boxes, then 1tp will negotiate a TURN session via a shared relay server.  
-
-## Features
-- stream based API, highly inspired by node's net API
-- current version includes UDP, TCP and TURN connectors -- extending UDP with hole punching + integrating other transports such as WebRTC, websockets, GCM and tor is WiP.
-- connection setup mechanism tries to select the 'cheapest' transport
-
-## Install
-
-```
-npm install 1tp
-```
-
-## Usage
-
-```js
 'use strict'
 
-var onetp = require('1tp')
+var onetp = require('../index')
 var net = onetp.net
 
-// 1tp transports
 var UdpTransport = onetp.transports.udp
 var TcpTransport = onetp.transports.tcp
 var TurnTransport = onetp.transports.turn
@@ -76,7 +53,3 @@ onetpServer.on('listening', function () {
 })
 // launch server
 onetpServer.listen()
-```
-
-## Examples
-See examples directory.
