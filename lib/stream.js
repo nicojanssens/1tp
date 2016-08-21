nicojanssens/1tp

commit ccb46dc53cd56e7e14e7f0400bf77bc5f346eb3f

attach error handlers to pipe statements
this fixes a range of issues where errors occur in underlying transport protocols that cause node to crash

e.g., dns resolution error from issue #17 but also TCP ECONNRESET errors

lib/stream.js
@@ -44,8 +44,8 @@ inherits(ProxyStream, Duplex)
 
 ProxyStream.prototype.connectStream = function (stream) {
   this._connectedStream = stream
-  this._outboundPassThrough.pipe(this._connectedStream)
-  this._connectedStream.pipe(this._inboundPassThrough)
+  this._outboundPassThrough.on('error', this._error).pipe(this._connectedStream).on('error', this._error)
+  this._connectedStream.on('error', this._error).pipe(this._inboundPassThrough).on('error', this._error)
 }
 
 ProxyStream.prototype._read = function (size) {

lib/transports/streams/netstring.js
@@ -54,7 +54,7 @@ inherits(NetStringStream, Duplex)
 
 NetStringStream.prototype.attachToEncoder = function (stream) {
   var self = this
-  this.encoder.pipe(stream)
+  this.encoder.on('error', this._error).pipe(stream).on('error', this._error)
   this.encoder.on('close', function () {
     if (typeof stream.destroy === 'function') {
       self._log.debug('closing destination stream')
@@ -69,7 +69,7 @@ NetStringStream.prototype.attachToEncoder = function (stream) {
 
 NetStringStream.prototype.attachToDecoder = function (stream) {
   var self = this
-  stream.pipe(this.decoder)
+  stream.on('error', this._error).pipe(this.decoder).on('error', this._error)
   stream.on('close', function () {
     self._log.debug('destination stream closed')
     self.destroy()

commit 6ae197f4efea996f9cb41eddb6b94d4a0db8f5b4

fixed bug introduced by commit ccb46dc

lib/stream.js
@@ -2,6 +2,7 @@
 
 var Duplex = require('stream').Duplex
 var inherits = require('util').inherits
+var myUtils = require('./utils')
 var PassThrough = require('stream').PassThrough
 var timers = require('timers')
 var winston = require('winston')
@@ -30,6 +31,8 @@ function ProxyStream () {
     .on('end', function () {
       self.push(null) // EOF
     })
+  // register _error handler
+  myUtils.mixinEventEmitterErrorFunction(this)
   // done
   this._log.debug('created proxy stream.')
 }