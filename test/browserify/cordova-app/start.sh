2beaf4e6a7f41dd3ac4aed2627bb45be7b7bbf16
Nico Janssens <nico.b.janssens@gmail.com>
2016-09-02 23:42:56 +0200

    minor update cordova-app configs

--- test/browserify/cordova-app/start.sh
+++ test/browserify/cordova-app/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 cordova platform add ios
-cordova plugin add --save https://github.com/MobileChromeApps/cordova-plugin-chrome-apps-sockets-udp
-cordova plugin add --save https://github.com/MobileChromeApps/cordova-plugin-chrome-apps-sockets-tcp
-cordova plugin add --save https://github.com/MobileChromeApps/cordova-plugin-chrome-apps-sockets-tcpserver
-cordova build ios
-cordova emulate ios
+cordova build ios --device
+cordova run --debug --emulator ios
+#cordova run ios
