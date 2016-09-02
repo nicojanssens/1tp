b6e40caba7b849558d3a0cede05b32a5326a179c
Nico Janssens <nico.b.janssens@gmail.com>
2016-09-02 23:40:54 +0200

    bug fix

--- test/browserify/client.js
+++ test/browserify/client.js
@@ -1,14 +1,5 @@
 'use strict'
 
-var net = require('../../lib/net')
-
-var onetpTransports = require('../../lib/transports')
-var TcpTransport = onetpTransports.tcp
-var UdpTransport = onetpTransports.udp
-var TurnTransport = onetpTransports.turn
-var TurnProtocols = require('turn-js').transports
-var WebSocketSignaling = require('../../lib/signaling').websocket
-
 var serverInfo = process.env.onetpServerAddress
 var turnAddr = process.env.turnAddr
 var turnPort = process.env.turnPort
@@ -23,29 +14,38 @@ console.log('turn user: ' + turnUser)
 console.log('turn password: ' + turnPwd)
 console.log('1tp registrar: ' + registrar)
 
-var onetpClient
+function runTest() {
+  var net = require('../../lib/net')
 
-function done(error) {
-  var message = (error === undefined)? 'done': error
-  onetpClient.write(message)
-}
+  var onetpTransports = require('../../lib/transports')
+  var TcpTransport = onetpTransports.tcp
+  var UdpTransport = onetpTransports.udp
+  var TurnTransport = onetpTransports.turn
+  var TurnProtocols = require('turn-js').transports
+  var WebSocketSignaling = require('../../lib/signaling').websocket
 
-function launchOneTpClient() {
+  var onetpClient
+
+  function done(error) {
+    var message = (error === undefined)? 'done': error
+    onetpClient.write(message)
+  }
+
   var transports = []
   transports.push(new UdpTransport())
-//  transports.push(new TcpTransport())
-  transports.push(
-    new TurnTransport({
-      turnServer: turnAddr,
-      turnPort: turnPort,
-      turnProtocol: new TurnProtocols.UDP(),
-      turnUsername: turnUser,
-      turnPassword: turnPwd,
-      signaling: new WebSocketSignaling({
-        url: registrar
+  //transports.push(new TcpTransport())
+    transports.push(
+      new TurnTransport({
+        turnServer: turnAddr,
+        turnPort: turnPort,
+        turnProtocol: new TurnProtocols.UDP(),
+        turnUsername: turnUser,
+        turnPassword: turnPwd,
+        signaling: new WebSocketSignaling({
+          url: registrar
+        })
       })
-    })
-  )
+    )
   onetpClient = net.createConnection(serverInfo, transports, function () {
     console.log('connection established')
     onetpClient.on('data', function (data) {
@@ -56,9 +56,14 @@ function launchOneTpClient() {
   })
 }
 
+function onDeviceReady() {
+  console.log('device ready')
+  runTest()
+}
+
 // start test
 if (window.cordova === undefined) {
-  launchOneTpClient()
+  runTest()
 } else {
-  document.addEventListener('deviceready', launchOneTpClient, false)
+  document.addEventListener('deviceready', onDeviceReady, false)
 }
