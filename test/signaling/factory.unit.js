31713db975296e3fef11c443ed6752a155f888f8
Nico Janssens <nico.b.janssens@gmail.com>
2016-11-01 19:42:13 +0100

    grouping all onetp signaling logic

---
+++ test/signaling/factory.unit.js
@@ -0,0 +1,88 @@
+'use strict'
+
+var myUtils = require('../../lib/utils')
+var SignalingFactory = require('../../lib/signaling/in-band/factory')
+
+var chai = require('chai')
+var expect = chai.expect
+
+var projectVersion = require('../../package.json').version
+
+describe('Signaling factory', function () {
+  this.timeout(2000)
+
+  var signalingFactory = new SignalingFactory()
+
+  it('should correctly create and parse a SYN message', function () {
+    var sessionId = myUtils.generateSessionId()
+    var transactionId = myUtils.generateTransactionId()
+    var data = signalingFactory.createSynPacket(sessionId, transactionId)
+    expect(data).to.not.be.undefined
+    var result = signalingFactory.parse(data)
+    expect(result.type).to.equal(SignalingFactory.MESSAGE.SYN)
+    expect(result.version).to.equal(projectVersion)
+    expect(result.sessionId).to.equal(sessionId)
+    expect(result.transactionId).to.equal(transactionId)
+  })
+
+  it('should correctly create and parse a SYN-ACK message', function () {
+    var sessionId = myUtils.generateSessionId()
+    var transactionId = myUtils.generateTransactionId()
+    var data = signalingFactory.createSynAckPacket(sessionId, transactionId)
+    expect(data).to.not.be.undefined
+    var result = signalingFactory.parse(data)
+    expect(result.type).to.equal(SignalingFactory.MESSAGE.SYN_ACK)
+    expect(result.version).to.equal(projectVersion)
+    expect(result.sessionId).to.equal(sessionId)
+    expect(result.transactionId).to.equal(transactionId)
+  })
+
+  it('should correctly create and parse a ACK message', function () {
+    var sessionId = myUtils.generateSessionId()
+    var transactionId = myUtils.generateTransactionId()
+    var data = signalingFactory.createAckPacket(sessionId, transactionId)
+    expect(data).to.not.be.undefined
+    var result = signalingFactory.parse(data)
+    expect(result.type).to.equal(SignalingFactory.MESSAGE.ACK)
+    expect(result.version).to.equal(projectVersion)
+    expect(result.sessionId).to.equal(sessionId)
+    expect(result.transactionId).to.equal(transactionId)
+  })
+
+  it('should correctly create and parse a DATA message', function () {
+    var chunk = new Buffer('DATA')
+    var sessionId = myUtils.generateSessionId()
+    var data = signalingFactory.createDataPacket(sessionId, chunk)
+    expect(data).to.not.be.undefined
+    var result = signalingFactory.parse(data)
+    expect(result.type).to.equal(SignalingFactory.MESSAGE.DATA)
+    expect(result.version).to.equal(projectVersion)
+    expect(result.sessionId).to.equal(sessionId)
+    expect(result.bytes).to.deep.equal(chunk)
+  })
+
+  it('should correctly create and parse a FIN message', function () {
+    var sessionId = myUtils.generateSessionId()
+    var transactionId = myUtils.generateTransactionId()
+    var data = signalingFactory.createFinPacket(sessionId, transactionId)
+    expect(data).to.not.be.undefined
+    var result = signalingFactory.parse(data)
+    expect(result.type).to.equal(SignalingFactory.MESSAGE.FIN)
+    expect(result.version).to.equal(projectVersion)
+    expect(result.sessionId).to.equal(sessionId)
+    expect(result.transactionId).to.equal(transactionId)
+  })
+
+  it('should correctly create and parse a RST message', function () {
+    var sessionId = myUtils.generateSessionId()
+    var transactionId = myUtils.generateTransactionId()
+    var data = signalingFactory.createRstPacket(sessionId, transactionId)
+    expect(data).to.not.be.undefined
+    var result = signalingFactory.parse(data)
+    expect(result.type).to.equal(SignalingFactory.MESSAGE.RST)
+    expect(result.version).to.equal(projectVersion)
+    expect(result.sessionId).to.equal(sessionId)
+    expect(result.transactionId).to.equal(transactionId)
+  })
+
+})
