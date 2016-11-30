a9cec0d262dc84d74e3a66ed1fef0d5a9fa31324
Nico Janssens <nico.b.janssens@gmail.com>
2016-11-30 21:01:59 +0100

    more tests, more bug fixes

--- test/transports/tcp.unit.js
+++ test/transports/tcp.unit.js
@@ -68,7 +68,7 @@ describe('tcp transport', function () {
       done)
   })
 
-  it('should correctly deal with a handshake timeout', function (done) {
+  it('should abort session when server is unreachable', function (done) {
     var clientSocket = new TcpTransport()
     var connectionInfo = {
       transportType: 'tcp',
@@ -85,6 +85,29 @@ describe('tcp transport', function () {
       .catch(function (error) {
         expect(error.code).to.be.a('string')
         expect(error.code).to.equal('ECONNREFUSED')
+        // test if there are no more sessions left
+        expect(Object.keys(clientSocket._connectingSockets).length).to.equal(0)
+        done()
+      })
+  })
+
+  it('should correctly deal with a handshake timeout', function (done) {
+    var clientSocket = new TcpTransport({connectTimeout: 1})
+    var connectionInfo = {
+      transportType: 'tcp',
+      transportInfo: {
+        address: '127.0.0.1',
+        port: 10004
+      }
+    }
+    clientSocket.connectP(connectionInfo)
+      .then(function (stream) {
+        var errorMsg = 'not expecting to receive connected stream ' + stream
+        done(errorMsg)
+      })
+      .catch(function (error) {
+        expect(error.message).to.be.a('string')
+        expect(['connect ECONNREFUSED 127.0.0.1:10004', 'handshake aborted']).to.include(error.message)
         // test if there are no more sessions left
         expect(Object.keys(clientSocket._connectingSockets).length).to.equal(0)
         done()
