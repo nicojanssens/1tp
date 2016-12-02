dea3db00baa631174f3340aeac44c677c32df817
Nico Janssens <nico.b.janssens@gmail.com>
2016-12-02 11:46:42 +0100

    added handshake timeout test + fixed bugs

--- test/transports/webrtc.unit.js
+++ test/transports/webrtc.unit.js
@@ -6,6 +6,9 @@ var WebRtcTransport = require('../../index').transports.webrtc
 var LocalSignaling = require('../../index').signaling.local
 var WebSocketSignaling = require('../../index').signaling.websocket
 
+var chai = require('chai')
+var expect = chai.expect
+
 // var turnAddr = process.env.TURN_ADDR
 // var turnPort = process.env.TURN_PORT
 // var turnUser = process.env.TURN_USER
@@ -17,7 +20,7 @@ if (!registrar) {
 }
 
 describe('webrtc transport', function () {
-  this.timeout(30000)
+  this.timeout(10000)
 
   it('should return echo messages using local signaling', function (done) {
     var localSignaling = new LocalSignaling()
@@ -92,5 +95,34 @@ describe('webrtc transport', function () {
         done(error)
       }
     )
+  })
+
+  it('should correctly deal with a handshake timeout', function (done) {
+    var clientSocket = new WebRtcTransport({
+      config: { iceServers: [ { url: 'stun:23.21.150.121' } ] },
+      signaling: new WebSocketSignaling({uid: 'foo', url: registrar}),
+      connectTimeout: 1000
+    })
+    var connectionInfo = {
+      transportType: 'webrtc',
+      transportInfo: {
+        type: 'websocket-signaling',
+        uid: 'bar',
+        url: 'http://1tp-registrar.microminion.io/'
+      }
+    }
+    clientSocket.connectP(connectionInfo)
+      .then(function (stream) {
+        var errorMsg = 'not expecting to receive connected stream ' + stream
+        done(errorMsg)
+      })
+      .catch(function (error) {
+        expect(error.message).to.be.a('string')
+        expect(error.message).to.equal('handshake aborted')
+        // test if there are no more sessions left
+        expect(Object.keys(clientSocket._sessions).length).to.equal(0)
+        expect(Object.keys(clientSocket._connectingPeers).length).to.equal(0)
+        done()
+      })
   })
 })
