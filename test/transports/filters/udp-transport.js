1c03dfebf694ef4696b4caa9bcb77463f0e03fd8
Nico Janssens <nico.b.janssens@gmail.com>
2017-01-09 13:55:40 +0100

    update length signaling type bytes

--- test/transports/filters/udp-transport.js
+++ test/transports/filters/udp-transport.js
@@ -20,14 +20,14 @@ function FilteringUdpTransport (args) {
 util.inherits(FilteringUdpTransport, UdpTransport)
 
 FilteringUdpTransport.prototype._onIncomingBytes = function (bytes, rinfo) {
-  var message = signalingFactory.parse(bytes)
+  var message = signalingFactory.parseTransportPacket(bytes)
   // if message passes the filter test
   if (this.filter(message)) {
-    console.log('FilteringUdpTransport -- IGNORING message ' + message.type.toString(16) + ' ' + JSON.stringify(message) + ' from ' + JSON.stringify(rinfo))
+    console.log('FilteringUdpTransport -- IGNORING message ' + message.type + ' ' + JSON.stringify(message) + ' from ' + JSON.stringify(rinfo))
     // then return -- dropping request on the floor
     return
   }
-  // console.log('FilteringUdpTransport -- PROCESSING message ' + message.type.toString(16) + ' ' + JSON.stringify(message) + ' from ' + JSON.stringify(rinfo))
+  // console.log('FilteringUdpTransport -- PROCESSING message ' + message.type + ' ' + JSON.stringify(message) + ' from ' + JSON.stringify(rinfo))
   FilteringUdpTransport.super_.prototype._onIncomingBytes.call(this, bytes, rinfo)
 }
 
