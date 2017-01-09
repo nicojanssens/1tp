a7eb5257c16016db85bb8589f3efefcca70fbd5e
Nico Janssens <nico.b.janssens@gmail.com>
2017-01-09 13:13:58 +0100

    added socket handshake when mutliple connections are established in parallel

--- test/session/proxy.unit.js
+++ test/session/proxy.unit.js
@@ -1,7 +1,11 @@
 'use strict'
 
+var duplexify = require('duplexify')
 var PassThrough = require('stream').PassThrough
 var ProxyStream = require('../../lib/stream')
+var signalingFactory = require('../../lib/signaling/in-band/factory')
+var Transform = require('stream').Transform
+var util = require('util')
 
 var chai = require('chai')
 var expect = chai.expect
@@ -45,6 +49,186 @@ describe('Testing proxy stream', function () {
       var testMessage = 'test message ' + i
       proxy.write(testMessage)
     }
+  })
+
+  it('should correctly execute handshake between two streams', function (done) {
+    var serverStreamReady = false
+    // create client proxy
+    var clientStream = new ProxyStream()
+    // create server proxy
+    var serverStream = new ProxyStream()
+    // create connector streams
+    var downStreamPipe = new FiteringPassThrough()
+    var upStreamPipe = new FiteringPassThrough()
+    var clientConnector = duplexify(downStreamPipe, upStreamPipe)
+    var serverConnector = duplexify(upStreamPipe, downStreamPipe)
+    // connect client and server streams
+    clientStream.connectStream(clientConnector)
+    serverStream.connectStream(serverConnector)
+    // init handshake
+    serverStream._waitForHandshakeP()
+      .then(function (connected) {
+        expect(connected).to.be.true
+        serverStreamReady = true
+      })
+    clientStream._initHandshakeP()
+      .then(function () {
+        expect(serverStreamReady).to.be.true
+        done()
+      })
+    // complain when other events are fired
+    clientStream.on('data', function () {
+      done("client stream should not receive DATA event")
+    })
+    clientStream.on('end', function () {
+      done("client stream should not receive END event")
+    })
+    clientStream.on('finish', function () {
+      done("client stream should not receive FINISH event")
+    })
+    clientStream.on('close', function () {
+      done("client stream should not receive CLOSE event")
+    })
+    clientStream.on('error', function (error) {
+      done(error)
+    })
+    serverStream.on('data', function () {
+      done("server stream should not receive DATA event")
+    })
+    serverStream.on('end', function () {
+      done("server stream should not receive END event")
+    })
+    serverStream.on('finish', function () {
+      done("server stream should not receive FINISH event")
+    })
+    serverStream.on('close', function () {
+      done("server stream should not receive CLOSE event")
+    })
+    serverStream.on('error', function (error) {
+      done(error)
+    })
+  })
+
+  it('should correctly handle PING losses while executing a handshake between two streams', function (done) {
+    // create client proxy
+    var clientStream = new ProxyStream()
+    // create server proxy
+    var serverStream = new ProxyStream()
+    // create connector streams
+    var downStreamPipe = new FiteringPassThrough()
+    downStreamPipe.dropMessage(signalingFactory.MESSAGE.PING)
+    var upStreamPipe = new FiteringPassThrough()
+    var clientConnector = duplexify(downStreamPipe, upStreamPipe)
+    var serverConnector = duplexify(upStreamPipe, downStreamPipe)
+    // connect client and server streams
+    clientStream.connectStream(clientConnector)
+    serverStream.connectStream(serverConnector)
+    // init handshake
+    serverStream._waitForHandshakeP()
+      .then(function (connected) {
+        expect(connected).to.be.true
+      })
+    clientStream._initHandshakeP()
+      .then(function () {
+        done("client stream should not succeed in handshake participation")
+      })
+      .catch(function (error) {
+        expect(error).to.not.be.undefined
+        done()
+      })
+    // complain when other events are fired
+    clientStream.on('data', function () {
+      done("client stream should not receive DATA event")
+    })
+    clientStream.on('end', function () {
+      done("client stream should not receive END event")
+    })
+    clientStream.on('finish', function () {
+      done("client stream should not receive FINISH event")
+    })
+    clientStream.on('close', function () {
+      done("client stream should not receive CLOSE event")
+    })
+    clientStream.on('error', function (error) {
+      done(error)
+    })
+    serverStream.on('data', function () {
+      done("server stream should not receive DATA event")
+    })
+    serverStream.on('end', function () {
+      done("server stream should not receive END event")
+    })
+    serverStream.on('finish', function () {
+      done("server stream should not receive FINISH event")
+    })
+    serverStream.on('close', function () {
+      done("server stream should not receive CLOSE event")
+    })
+    serverStream.on('error', function (error) {
+      done(error)
+    })
+  })
+
+  it('should correctly handle PONG losses while executing a handshake between two streams', function (done) {
+    var serverStreamReady = false
+    // create client proxy
+    var clientStream = new ProxyStream()
+    // create server proxy
+    var serverStream = new ProxyStream()
+    // create connector streams
+    var downStreamPipe = new FiteringPassThrough()
+    var upStreamPipe = new FiteringPassThrough()
+    upStreamPipe.dropMessage(signalingFactory.MESSAGE.PONG)
+    var clientConnector = duplexify(downStreamPipe, upStreamPipe)
+    var serverConnector = duplexify(upStreamPipe, downStreamPipe)
+    // connect client and server streams
+    clientStream.connectStream(clientConnector)
+    serverStream.connectStream(serverConnector)
+    // init handshake
+    serverStream._waitForHandshakeP()
+      .then(function (connected) {
+        expect(connected).to.be.true
+        serverStreamReady = true
+      })
+    clientStream._initHandshakeP()
+      .then(function () {
+        done("client stream should not succeed in handshake participation")
+      })
+      .catch(function (error) {
+        expect(error).to.not.be.undefined
+        done()
+      })
+    // complain when other events are fired
+    clientStream.on('data', function () {
+      done("client stream should not receive DATA event")
+    })
+    clientStream.on('end', function () {
+      done("client stream should not receive END event")
+    })
+    clientStream.on('finish', function () {
+      done("client stream should not receive FINISH event")
+    })
+    clientStream.on('close', function () {
+      done("client stream should not receive CLOSE event")
+    })
+    clientStream.on('error', function (error) {
+      done(error)
+    })
+    serverStream.on('data', function () {
+      done("server stream should not receive DATA event")
+    })
+    serverStream.on('end', function () {
+      done("server stream should not receive END event")
+    })
+    serverStream.on('finish', function () {
+      done("server stream should not receive FINISH event")
+    })
+    serverStream.on('close', function () {
+      done("server stream should not receive CLOSE event")
+    })
+    serverStream.on('error', function (error) {
+      done(error)
+    })
   })
 
   it('should correctly process setTimeout requests', function (done) {
@@ -97,3 +281,43 @@ describe('Testing proxy stream', function () {
     }, 500)
   })
 })
+
+function FiteringPassThrough (options) {
+  if (!(this instanceof FiteringPassThrough)) {
+    return new FiteringPassThrough(options)
+  }
+  Transform.call(this, options)
+  // init
+  this.filter = function () {
+    // drop no messages
+    return false
+  }
+}
+
+util.inherits(FiteringPassThrough, Transform)
+
+FiteringPassThrough.prototype._transform = function(bytes, encoding, cb) {
+  var packet = signalingFactory.parseSocketPacket(bytes)
+  // if message type needs to be filtered
+  if (this.filter(packet)) {
+    // ignore packet
+    console.log('FiteringPassThrough -- IGNORING message ' + packet.type)
+    cb()
+    return
+  }
+  // else proceed
+  var self = this
+  process.nextTick(function () {
+    // console.log('FiteringPassThrough -- PROCESSING message ' + packet.type)
+    self.push(bytes)
+    cb()
+  })
+}
+
+FiteringPassThrough.prototype.dropMessage = function (packetType) {
+  this.filter = function (packet) {
+    if (packet.type === packetType) {
+      return true
+    }
+  }
+}
